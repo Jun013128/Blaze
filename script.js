c704d990f42538a18f3a3a423b34427fac2cbbae
Jun013128 <112373928+Jun013128@users.noreply.github.com>
2026-01-22 00:11:35 +0900

    サイトを開いたときのアニメーションの追加。ハンバーガーメニューのアニメーションの追加。

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const
   overlay = document.querySelector('#overlay'),
   menuLabel = document.querySelector('#menulabel'),
   els = document.querySelectorAll('.fade-in');
+  loader = document.querySelector('.start');
   
 const hamburgerClose = () => {
   document.body.classList.remove('is-active');
@@ -48,3 +49,30 @@ const observer = new IntersectionObserver(entries => {
   threshold: 0
 });
 els.forEach(el => observer.observe(el));
+
+
+window.addEventListener('load', () => {
+  if (loader) {
+    // 1. すでにアニメーションを見たかチェック
+    if (!sessionStorage.getItem('visited')) {
+      // --- 初めてのアクセス（アニメーションを実行） ---
+
+      // 3.5秒後にフェードアウト
+      setTimeout(() => {
+        loader.classList.add('is-loaded');
+        // 「見た」という記録を残す
+        sessionStorage.setItem('visited', 'true');
+      }, 3500);
+
+    } else {
+      // --- 2回目以降のアクセス（即座に非表示） ---
+
+      // 待ち時間なしで、即座に非表示にする
+      loader.style.opacity = '0';      // 透明にする
+      loader.style.visibility = 'hidden'; // クリックできないようにする
+      loader.style.transition = 'none'; // アニメーションを無効化
+      loader.classList.add('is-loaded');
+    }
+  }
+});
+  
